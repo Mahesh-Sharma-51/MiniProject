899a23cd4895d3c0ddfd34752f0e213b8538bae8
Mahesh-Sharma-51 <22bcs051@smvdu.ac.in>
2024-10-16 03:26:35 +0530

    Main Page Done

---
+++ app.js
@@ -0,0 +1,7 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    const loginButton = document.querySelector(".login-btn");
+    loginButton.addEventListener("click", function() {
+        alert("PROCEED TO LOGIN SECTION");
+    });
+});
